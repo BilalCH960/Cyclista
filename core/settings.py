a15b2ac274127a1170e2a028f25fe2dd0d52e237
Bilal <mohdbilalhamzac@gmail.com>
2024-07-03 16:05:48 +0530

    error page corrected

--- core/settings.py
+++ core/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
-
+DEBUG = config("DEBUG", cast=bool)
+print(type(DEBUG))
 ALLOWED_HOSTS = ["*"]
 
 
